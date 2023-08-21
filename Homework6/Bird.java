64735c421e059c2d60a77d1bdb4989402f8ccff1
phoenix12120707 <139230937+phoenix12120707@users.noreply.github.com>
2023-08-21 15:29:53 +0300

    Add files via upload

--- Homework6/Bird.java
+++ Homework6/Bird.java
@@ -1,20 +1,37 @@
 package Homework6;
 
-public class Bird extends Animal{
+public class Bird extends Animal {
     @Override
     public void play() {
         System.out.println("Bird play");
 
     }
-    public void fly (){
+
+    public void fly() {
         System.out.println("Bird fly");
     }
-    public void sing(){
+
+    public void sing() {
         System.out.println("Bird sing");
     }
 
+    public void sing(int birdCount) {
+        if (birdCount > 1 && birdCount <= 4) {
+            System.out.println("Birds singing noisy");
+        }
+        else if (birdCount == 1) {
+            System.out.println("There is only one beautiful bird singer");
+
+        }
+        else {
+            System.out.println("There are too many birds, and it is very loud");
+        }
+
+
+        }
+
     @Override
     public void makeSomeNoise() {
-        System.out.println("churulik churulik");
+        System.out.println("Birds say Hi");
     }
 }
